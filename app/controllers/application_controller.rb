84c92bec83349202df378e00a83adc908e605370
ThisIsTyDell <milleb94@gmail.com>
2016-09-06 15:05:21 -0500

    enable sessions and set session secret to enable logging in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :views, 'app/views'    
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "mic_check"   
   end
 
   get '/' do
