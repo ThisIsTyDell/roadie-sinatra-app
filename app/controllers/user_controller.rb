9d22b501afe80e07081c4270c8c6a2c1224ccbeb
ThisIsTyDell <milleb94@gmail.com>
2016-09-07 16:31:40 -0500

    use form data params to search for User and assign to local variable user, then check to see if password authenticates and finds a user and direct user to homepage placeholder

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,12 +9,22 @@ class UserController < ApplicationController
       redirect to '/signup'
     else
       user = User.create(params)
-      session[:id] = user.id
+      session[:user_id] = user.id
       redirect to '/'
     end
   end
 
   get '/login' do
     erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/'
+    else
+      erb :'users/login'
+    end
   end
 end
