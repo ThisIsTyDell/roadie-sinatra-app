94da7b5b3db24c0d06dc76f1a34bb9babfbe8e5b
ThisIsTyDell <milleb94@gmail.com>
2016-09-07 15:33:15 -0500

    create new user from form params hash and save into instance variable @user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,6 @@ class UserController < ApplicationController
   end
 
   post '/signup' do
-    "#{params}"
+    @user = User.create(username: params[:username], email: params[:email], password_digest: params[:password])
   end
 end
