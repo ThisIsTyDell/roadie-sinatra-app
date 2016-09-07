38bd748e391fde3a33f6ac08dfe29ee5b770ec0c
ThisIsTyDell <milleb94@gmail.com>
2016-09-07 15:45:58 -0500

    add validation uniqueness to only allow unique email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   has_many :trucks
   has_many :items, through: :trucks
 
+  validates :username, uniqueness: true
+  validated :email, uniqueness: true
   has_secure_password
 end
