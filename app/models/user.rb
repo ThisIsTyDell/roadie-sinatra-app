e483cb989e95bf9a585d4d08e71e170f97bc14f0
ThisIsTyDell <milleb94@gmail.com>
2016-09-07 15:46:31 -0500

    fix typo in validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   has_many :items, through: :trucks
 
   validates :username, uniqueness: true
-  validated :email, uniqueness: true
+  validates :email, uniqueness: true
   has_secure_password
 end
