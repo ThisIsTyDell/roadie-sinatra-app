f7df564a772175686771e1a76e025ea27854444b
ThisIsTyDell <milleb94@gmail.com>
2016-09-07 16:42:40 -0500

    add helper method logged_in? to return true or false of user is logged in or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,10 @@ class ApplicationController < Sinatra::Base
     def current_user
       @current_user ||= User.find_by_id(session[:user_id])
     end
+
+    def logged_in?
+      !!current_user
+    end
   end
 
 end
