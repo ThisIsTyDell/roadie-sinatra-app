309084d01359a0cc463bbed6d54a832db9c87671
ThisIsTyDell <milleb94@gmail.com>
2016-09-11 15:12:42 -0500

    add  bootstrap library

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "mic_check" 
+    set :session_secret, "mic_check"
     end  
   
   get '/' do
