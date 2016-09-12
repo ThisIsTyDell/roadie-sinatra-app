c0c3b4a8afdef29bf9499d64277ccd497d4b1ec8
ThisIsTyDell <milleb94@gmail.com>
2016-09-12 14:13:47 -0500

    add use Rack::Flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
+  use Rack::Flash
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "mic_check"
-    end  
+  end  
   
   get '/' do
     erb :index
